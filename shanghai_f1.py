182e16f4ddd7446fcb5a82e788fb391c5633a481
Jasper Van Zeir <jaspervanzeir1@gmail.com>
2025-05-31 22:16:20 +0200

    update

--- shanghai_f1.py
+++
@@ -1,5 +0,0 @@
-from race_predictor import predict_race
-
-if __name__ == "__main__":
-    results = predict_race("Chinese Grand Prix")
-    print(results[["Driver", "Team", "Grid", "Final_Position"]])
