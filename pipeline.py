558efa656d80bf71cfd70180193de7696a3729ab
R3D-R4V3N <jaspervanzeir1@gmail.com>
2025-06-02 13:56:35 +0200

    train data to 2022 iso 2021

--- pipeline.py
+++ pipeline.py
@@ -81,7 +81,7 @@ def predict_race(
         if os.path.exists(cache_data_path):
             race_data = pd.read_parquet(cache_data_path)
         else:
-            seasons = list(range(2021, year + 1))
+            seasons = list(range(2022, year + 1))
             overtake_map = _load_overtake_stats()
             race_data = _load_historical_data(seasons, overtake_map, limit_rounds)
             race_data = _engineer_features(race_data)
@@ -90,12 +90,12 @@ def predict_race(
         results = _predict_with_existing_model(model, race_data, grand_prix, year)
         return (results, None) if debug else results
 
-    seasons = list(range(2021, year + 1))
+    seasons = list(range(2022, year + 1))
 
     overtake_map = _load_overtake_stats()
     if compute_overtakes:
         try:
-            years_for_avg = list(range(max(2021, year - 3), year))
+            years_for_avg = list(range(max(2022, year - 3), year))
             avg = average_overtakes(grand_prix, years_for_avg)
             overtake_map[grand_prix] = avg
         except Exception as err:
